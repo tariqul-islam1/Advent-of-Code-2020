b6a059adf72a03b5555c95a5e57febe97d074752
Tariqul <tariqul.islam@appland.se>
2020-12-29 17:35:18 +0100

    Solved day 3 part 2

--- java1/src/main/java/com/appland/java1/Main.java
+++ java1/src/main/java/com/appland/java1/Main.java
@@ -7,6 +7,7 @@ package com.appland.java1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -15,44 +16,67 @@ import java.util.Scanner;
  */
 public class Main {
 
-    static int countOfTrees = 0;
-
     public static void main(String... args) {
+        int[] rightValues = new int[]{1, 3, 5, 7, 1};
+        int[] downValues = new int[]{1, 1, 1, 1, 2};
+        int[] countOfTrees = new int[rightValues.length];
+        long treesMultiply = 1;
+
+        for (int i = 0; i < rightValues.length; i++) {
+            countOfTrees[i] = getCountOfTrees(rightValues[i], downValues[i]);
+        }
+        for (int n : countOfTrees) {
+            treesMultiply = treesMultiply * n;
+        }
+        System.out.println("" + Arrays.toString(countOfTrees));
+        System.out.println("" + treesMultiply);
+    }
+
+    static int getCountOfTrees(int nextRight, int nextDown) {
+        int trees = 0;
         try {
             Scanner fileContent = new Scanner(new File("src/main/java/com/appland/java1/input.txt")).useDelimiter("\\n");
-            int locationNow = 3;
+            int locationNow = 0;
             boolean shouldIncreaseByThree = false;
             int numberOfTrees = 0;
             String line = fileContent.next();
-            //System.out.println(line);
+//            System.out.println(line);
             while (fileContent.hasNext()) {
-                line = fileContent.next();
-                if (shouldIncreaseByThree) {
-                    line = getUpdatedLine(line, locationNow);
-                    locationNow = (locationNow + 3) % line.trim().length();
-                    shouldIncreaseByThree = false;
-                } else {
-                    shouldIncreaseByThree = true;
-                    line = getUpdatedLine(line, locationNow);
-                    locationNow = (locationNow + 3) % line.trim().length();
+                locationNow = (locationNow + nextRight) % line.trim().length();
+                line = getNextLine(fileContent, nextDown);
+                if (isTreeFound(line, locationNow)) {
+                    trees++;
                 }
-                //System.out.println(line);
+//                System.out.println(line);
                 //System.out.println("Location now: " + locationNow + ", " + line.charAt(locationNow));
             }
-            System.out.println("" + countOfTrees);
+            //System.out.println("" + countOfTrees);
 
         } catch (FileNotFoundException fnfe) {
         }
-
+        return trees;
     }
 
-    static String getUpdatedLine(String line, int location) {
-        if (line.charAt(location) == '#') {
-            countOfTrees++;
-            return line.substring(0, location) + 'X' + line.substring(location + 1);
-        } else {
-            return line.substring(0, location) + 'O' + line.substring(location + 1);
+    static String getNextLine(Scanner fileContent, int nextDown) {
+        int downCount = 0;
+        String line = "";
+        while (fileContent.hasNext() && ++downCount <= nextDown) {
+            line = fileContent.next();
         }
+        return line;
+    }
+
+    static boolean isTreeFound(String line, int location) {
+        if (line.charAt(location) == '#') {
+            return true;
+        }
+        return false;
+//        if (line.charAt(location) == '#') {
+//            countOfTrees++;
+//            return line.substring(0, location) + 'X' + line.substring(location + 1);
+//        } else {
+//            return line.substring(0, location) + 'O' + line.substring(location + 1);
+//        }
     }
 
 }
