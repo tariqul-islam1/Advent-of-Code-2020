e432864ddfc019710d0bb65e92df7ba7f97870f8
Tariqul Islam <toriq100@gmail.com>
2021-01-11 20:23:50 +0100

    Done with day 6

--- java1/src/main/java/com/appland/java1/Main.java
+++ java1/src/main/java/com/appland/java1/Main.java
@@ -8,7 +8,6 @@ package com.appland.java1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -20,54 +19,55 @@ public class Main {
     public static void main(String... args) throws FileNotFoundException {
         Scanner fileContent = new Scanner(new File("src/main/java/com/appland/java1/input.txt")).useDelimiter("\\n");
 
-        System.out.println("" + getHighestSeatId(fileContent));
+        System.out.println("" + getSumOfCounts(fileContent));
     }
 
-    private static String getHighestSeatId(Scanner fileContent) {
-        int seatId = -1;
-        ArrayList<Integer> ids = new ArrayList<>();
+    private static int getSumOfCounts(Scanner fileContent) {
+        int finalCount = 0;
+        ArrayList<String> persons = new ArrayList<>();
 
         while (fileContent.hasNext()) {
-            int f = 0;
-            int b = 127;
-            int l = 0;
-            int r = 7;
-            String input = fileContent.next();
-            for (int i = 0; i <= 6; i++) {
-                char c = input.charAt(i);
-                if (c == 'F') {
-                    b = (int) Math.floor((f + b) / 2.0);
-                } else if (c == 'B') {
-                    f = (int) Math.ceil((f + b) / 2.0);
-                }
-            }
-            for (int i = 7; i <= 9; i++) {
-                char c = input.charAt(i);
-                if (c == 'R') {
-                    l = (int) Math.ceil((l + r) / 2.0);
-                } else if (c == 'L') {
-                    r = (int) Math.floor((l + r) / 2.0);
-                }
-            }
-            if (seatId < ((f * 8) + l)) {
-                seatId = (f * 8) + l;
-            }
-            ids.add((f * 8) + l);
-//            System.out.println("seat id: " + ((f * 8) + l));
-
-        }
-
-        Collections.sort(ids);
-        //System.out.println("seat ids: " + ids.toString());
-        int prev = 0;
-        for (int i = 0; i < ids.size(); i++) {
-            if (prev + 1 == ids.get(i) - 1) {
-                return "" + (prev + 1);
+            String person = fileContent.next();
+            if (!person.equals("")) {
+                persons.add(person);
             } else {
-                prev = ids.get(i);
+                finalCount += getCountForGroup(persons);
+                persons = new ArrayList<>();
             }
         }
-        return "" + seatId;
+        finalCount += getCountForGroup(persons);
+
+        return finalCount;
+    }
+
+    private static int getCountForGroup(ArrayList<String> persons) {
+        String commonChars = "";
+        if (persons.size() == 1) {
+            return persons.get(0).length();
+        }
+        for (int i = 0; i < persons.size(); i++) {
+            if (i == 0) {
+                commonChars = getCommonChars(persons.get(i), persons.get(i + 1));
+                i = i + 1;
+            } else {
+                commonChars = getCommonChars(commonChars, persons.get(i));
+            }
+            if (commonChars.length() == 0) {
+                break;
+            }
+        }
+        return commonChars.length();
+    }
+
+    private static String getCommonChars(String a, String b) {
+        StringBuilder commons = new StringBuilder();
+
+        for (int i = 0; i < a.length(); i++) {
+            if (b.contains("" + a.charAt(i))) {
+                commons.append("" + a.charAt(i));
+            }
+        }
+        return commons.toString();
     }
 
 }
